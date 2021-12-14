4be47e5716aca742270bf3989774497526f86f19
Mayur Kashyap <43520553+mayurkashyap60@users.noreply.github.com>
2021-12-14 12:00:20 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,50 +1,83 @@
-// initializing all vaiables
-var slider = document.getElementById("slider");
-var sliderWidth = slider.offsetWidth;
-var slideList = document.getElementById("sliderWrap");
-var count = 1;
-var items = slideList.querySelectorAll("li").length;
-var prev = document.getElementById("prev");
-var next = document.getElementById("next");
-//create function for image slider
-var imageSlider = () => {
-  window.addEventListener("resize", () => {
-    sliderWidth = slider.offsetWidth;
+//initializing all variables
+
+const $slides = gsap.utils.toArray(".slide"); // to store all slide elements in array for sliding
+const $slidesImage = gsap.utils.toArray(".img"); // store all images in array for sliding
+const $buttonPrev = document.getElementById("prev"); // defined variable for previous slide
+const $buttonNext = document.getElementById("next"); // defined variable for next slide
+
+//initializing all variables
+
+let totalSlides = $slides.length; // here first we get all slide length in a totalSlides variable
+let currentSlide = 0; // take an variable and define it's state a zero for starting slide as a string from the zero.
+const slidesWrap = gsap.utils.wrap(0, totalSlides); // store index of an Array into a specified range in variable
+
+// variable for store their slide, directon and duration for sliding content
+const transitionInSlide = ({ slide, direction = 1, duration = 1 }) => {
+  //gsap.fromTo define for the starting and ending values for an animation
+  gsap.fromTo(
+    $slides[slide],
+    {
+      xPercent: direction > 0 ? 100 : -100,
+    },
+    {
+      xPercent: 0,
+      duration,
+    }
+  );
+
+  //gsap.fromTo define for the starting and ending values for an animation
+  gsap.fromTo(
+    $slidesImage[slide], // sliding all sides as an array starting from the zero to array length
+    {
+      xPercent: direction > 0 ? -100 : 100,
+    },
+
+    {
+      xPercent: 0,
+      duration,
+    }
+  );
+};
+
+// function for slide with translate therir postion for x-axis from 0 to 100
+const transitionOutSlide = ({ slide, direction = 1, duration = 1 }) => {
+  gsap.to($slides[slide], {
+    xPercent: direction > 0 ? -100 : 100,
+    duration,
   });
 
-  var prevSlide = () => {
-    if (count > 1) {
-      count = count - 2;
-      slideList.style.left = "-" + count * sliderWidth + "px";
-      count++;
-    } else if ((count = 1)) {
-      count = items - 1;
-      slideList.style.left = "-" + count * sliderWidth + "px";
-      count++;
-    }
-  };
-
-  var nextSlide = () => {
-    if (count < items) {
-      slideList.style.left = "-" + count * sliderWidth + "px";
-      count++;
-    } else if ((count = items)) {
-      slideList.style.left = "0px";
-      count = 1;
-    }
-  };
-
-  next.addEventListener("click", () => {
-    nextSlide();
-  });
-
-  prev.addEventListener("click", () => {
-    prevSlide();
+  // gsap.to define the destination or starting index value
+  gsap.to($slidesImage[slide], {
+    xPercent: direction > 0 ? 100 : -100,
+    duration,
   });
 };
 
-window.onload = () => {
-  imageSlider();
+// function for handling the slider's previous slide
+const handlePrev = () => {
+  const oldSlide = currentSlide;
+  currentSlide = slidesWrap(currentSlide - 1);
+  transitionInSlide({ slide: currentSlide, direction: -1 });
+  transitionOutSlide({ slide: oldSlide, direction: -1 });
 };
 
-console.log("app.js is running");
+// function for handling the slider's next slide
+const handleNext = () => {
+  const oldSlide = currentSlide;
+  currentSlide = slidesWrap(currentSlide + 1);
+  transitionInSlide({ slide: currentSlide });
+  transitionOutSlide({ slide: oldSlide });
+};
+
+$slides.forEach(($slide, index) => {
+  // define loop for starting from the zero with animation using array of index
+  if (index === currentSlide) {
+    transitionInSlide({ slide: index, duration: 0 });
+    return;
+  }
+
+  transitionOutSlide({ slide: index, duration: 0 });
+});
+
+$buttonPrev.addEventListener("click", handlePrev); // adding event listener form sliding previous slide
+$buttonNext.addEventListener("click", handleNext); // adding event listener form sliding next slide
