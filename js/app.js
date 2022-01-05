4071441236dcd8113641d679555a786612121645
Mayur Kashyap <43520553+mayurkashyap60@users.noreply.github.com>
2022-01-05 19:49:43 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,59 +1,20 @@
 // initializing all vaiables
-var $slider = document.getElementById("slider");
-const $sliderWidth = $slider.offsetWidth; // to get their size in pixel for sliding
-const $slideList = document.getElementById("sliderWrap");
-const $items = $slideList.querySelectorAll("li").length; // store all list in array for sliding
+const $slideList = $("#slider");
+const $items = $slideList.find("li"); // store all list in array for sliding
+const slidesNum = $items.length;
 const $buttonPrev = document.getElementById("prev"); // defined variable for previous slide
 const $buttonNext = document.getElementById("next"); // defined variable for next slide
-var count = 1; //to default value
 var isAutoPlay = false; //intialized variable for stopping autplay slider
+var previousSlide = null; //initialized variable by previous slide defined as a null or zero
+var currentSlide = 0; //initialized variable by current slide defined as 0
+var isBehave = false; //initialized variable by previous slide defined as a null
 
 //create function for load image slider
 var imageSlider = () => {
-  window.addEventListener("resize", () => {
-    $sliderWidth = $slider.offsetWidth;
+  TweenLite.set($items, {
+    left: "-100%",
   });
-
-  // function for handling the slider's next slide
-  nextSlide = () => {
-    if (count < $items) {
-      var abc = count++;
-      switch (abc) {
-        case 1:
-          TweenMax.to($slideList, { css: { left: -300 } });
-          break;
-        case 2:
-          TweenMax.to($slideList, { css: { left: -600 } });
-          break;
-        case 3:
-          TweenMax.to($slideList, { css: { left: -900 } });
-          break;
-        case 4:
-          TweenMax.to($slideList, { css: { left: -1200 } });
-          break;
-        default:
-          console.log("Not An Value");
-          break;
-      }
-    } else if ((count = $items)) {
-      TweenMax.to($slideList, { css: { left: 0 } });
-      // TweenMax.to($slideList, { x: 0 });
-      count = 1;
-    }
-  };
-
-  // function for handling the slider's previous slide
-  prevSlide = () => {
-    if (count > 1) {
-      count = count - 2;
-      $slideList.style.left = "-" + count * $sliderWidth + "px";
-      count++;
-    } else if ((count = 1)) {
-      count = $items - 1;
-      $slideList.style.left = "-" + count * $sliderWidth + "px";
-      count++;
-    }
-  };
+  renderSlider(0, 0);
 
   $buttonNext.addEventListener("click", () => {
     nextSlide();
@@ -63,9 +24,73 @@ var imageSlider = () => {
     prevSlide();
   });
 };
-// when browser body load function
-window.onload = () => {
-  imageSlider();
+
+function renderSlider(slideID, _time, _direction) {
+  if (!isBehave) {
+    isBehave = true;
+    previousSlide = currentSlide;
+    currentSlide = slideID;
+    var $prevSlide = $items.eq(previousSlide);
+    var $currentSlide = $items.eq(currentSlide);
+    var time = 1;
+    if (_time !== null) {
+      time = _time;
+    }
+    var direction = "next";
+    if (_direction != null) {
+      direction = _direction;
+    }
+    if (direction == "next") {
+      TweenLite.to($prevSlide, time, {
+        left: "-100%",
+      });
+      TweenLite.fromTo(
+        $currentSlide,
+        time,
+        {
+          left: "100%",
+        },
+        {
+          left: "0",
+        }
+      );
+    } else {
+      TweenLite.to($prevSlide, time, {
+        left: "100%",
+      });
+      TweenLite.fromTo(
+        $currentSlide,
+        time,
+        {
+          left: "-100%",
+        },
+        {
+          left: "0",
+        }
+      );
+    }
+    TweenLite.delayedCall(time, function () {
+      isBehave = false;
+    });
+  }
+}
+
+// function for handling the slider's next slide
+var nextSlide = () => {
+  var firstSlide = currentSlide + 1;
+  if (firstSlide >= slidesNum) {
+    firstSlide = 0;
+  }
+  renderSlider(firstSlide, 1, "next");
+};
+
+// function for handling the slider's previous slide
+var prevSlide = () => {
+  var firstSlide = currentSlide - 1;
+  if (firstSlide <= -1) {
+    firstSlide = slidesNum - 1;
+  }
+  renderSlider(firstSlide, 1, "prev");
 };
 
 //function for slider handling dealy function
@@ -103,3 +128,10 @@ const over = () => {
 const leave = () => {
   startAutoPlay();
 };
+
+// when browser body load function
+window.onload = () => {
+  imageSlider();
+};
+
+console.log("app.js is running");
